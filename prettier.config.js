67c034d15ed104e6b7d17370309d58af585d1351
hsukiasyan <h.sukiasyan@gethatch.com>
2022-09-11 10:40:04 +0200

    initial setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowPatterns: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+module.exports = options
